406769bf804a0c301ea130e6612575bbe18c684c
jmartinez <jcmartinez1925@gmail.com>
2019-09-12 23:27:41 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
+
+app.listen(PORT, function() {
+  console.log("Friend Finder app is listening on PORT: " + PORT);
+});
+
