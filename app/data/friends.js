dbde8f9148e5d31ed4ed39e688f998e4023f771c
jmartinez <jcmartinez1925@gmail.com>
2019-09-13 06:39:29 -0700

    add friends array

---
+++ app/data/friends.js
@@ -0,0 +1,99 @@
+var friends = [
+
+    {
+        name: "Meredith",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg1.looper.com%2Fimg%2Fgallery%2Fwhatever-happened-to-meredith-from-the-office%2Fintro-1526005235.jpg&imgrefurl=https%3A%2F%2Fwww.looper.com%2F121969%2Fwhatever-happened-meredith-office%2F&docid=IURWmMsV3HBhuM&tbnid=Jr3ShZGVbV8J8M%3A&vet=10ahUKEwir4vSC8s3kAhUF0KwKHZzjDsEQMwheKA0wDQ..i&w=780&h=439&bih=578&biw=1280&q=actors%20the%20office&ved=0ahUKEwir4vSC8s3kAhUF0KwKHZzjDsEQMwheKA0wDQ&iact=mrc&uact=8",
+        scores: [
+            "4",
+            "3",
+            "5",
+            "1",
+            "1",
+            "2",
+            "4",
+            "3",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "Dwight",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2Fc%2Fcd%2FDwight_Schrute.jpg%2F220px-Dwight_Schrute.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FDwight_Schrute&docid=NEv6D-sIMk8WAM&tbnid=WZE3oV19zhH8XM%3A&vet=10ahUKEwixl8H1883kAhUQS60KHVOZDVwQMwhfKAAwAA..i&w=220&h=311&bih=578&biw=1280&q=actors%20the%20office&ved=0ahUKEwixl8H1883kAhUQS60KHVOZDVwQMwhfKAAwAA&iact=mrc&uact=8",
+        scores: [
+            "3",
+            "2",
+            "5",
+            "3",
+            "1",
+            "2",
+            "5",
+            "4",
+            "4",
+            "2"
+        ]
+    },
+    {
+        name: "Jim",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.demilked.com%2Fmagazine%2Fwp-content%2Fuploads%2F2019%2F02%2F5c61377fb0b76-the-office-10-years-challenge-4-5c5da1fdbd75d__700.jpg&imgrefurl=https%3A%2F%2Fwww.demilked.com%2F10-year-challenge-the-office%2F&docid=12yK4-ZPGTuj-M&tbnid=eXKXm7mDXy0JRM%3A&vet=10ahUKEwjs0uSm9M3kAhUnjK0KHeQACccQMwhnKAgwCA..i&w=700&h=368&bih=578&biw=1280&q=actors%20the%20office&ved=0ahUKEwjs0uSm9M3kAhUnjK0KHeQACccQMwhnKAgwCA&iact=mrc&uact=8",
+        scores: [
+            "5",
+            "4",
+            "3",
+            "3",
+            "1",
+            "2",
+            "3",
+            "4",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Jen",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fthumb%2Fd%2Fd1%2FMelora_Hardin_as_Jan_Levinson.png%2F250px-Melora_Hardin_as_Jan_Levinson.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FJan_Levinson&docid=828B_WRjMSSm1M&tbnid=QIcvQwemyEShJM%3A&vet=10ahUKEwiip8ug9c3kAhUQbKwKHf-1AX4QMwhRKAAwAA..i&w=250&h=250&bih=578&biw=1280&q=actors%20the%20office&ved=0ahUKEwiip8ug9c3kAhUQbKwKHf-1AX4QMwhRKAAwAA&iact=mrc&uact=8",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5",
+            "2",
+            "4",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "Michael",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimgix.bustle.com%2Fuploads%2Fimage%2F2018%2F4%2F9%2F0e1587de-fd74-4e77-9264-62a0bf5e894a-nup_130296_0067.jpeg%3Fw%3D1020%26h%3D574%26fit%3Dcrop%26crop%3Dfaces%26auto%3Dformat%26q%3D70&imgrefurl=https%3A%2F%2Fwww.elitedaily.com%2Fp%2F10-secrets-about-the-office-that-even-the-biggest-michael-scott-fan-wont-know-8735316&docid=PEQ41JvIExONSM&tbnid=bujvX3NwyqY5YM%3A&vet=10ahUKEwiip8ug9c3kAhUQbKwKHf-1AX4QMwheKA0wDQ..i&w=1020&h=574&bih=578&biw=1280&q=actors%20the%20office&ved=0ahUKEwiip8ug9c3kAhUQbKwKHf-1AX4QMwheKA0wDQ&iact=mrc&uact=8",
+        scores: [
+            "5",
+            "5",
+            "3",
+            "2",
+            "1",
+            "2",
+            "3",
+            "4",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Brian",
+        photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F00%2F8e%2Fd2%2F008ed2c6e7e24ef7d4fc3a27add092c6.png&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F407364728763983438%2F&docid=1Efo2i1rKCU40M&tbnid=Lop9pbSlNzbRvM%3A&vet=10ahUKEwjw9cHv9c3kAhUEXawKHds_AkcQMwhbKAowCg..i&w=1054&h=1407&bih=578&biw=1280&q=actors%20the%20office&ved=0ahUKEwjw9cHv9c3kAhUEXawKHds_AkcQMwhbKAowCg&iact=mrc&uact=8",
+        scores: [
+            "2",
+            "2",
+            "3",
+            "4",
+            "1",
+            "2",
+            "5",
+            "4",
+            "3",
+            "2"
+        ]
+    },
+]
